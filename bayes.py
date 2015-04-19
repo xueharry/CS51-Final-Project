e18ad7aa2f12bc65cabfb1672109a421f1a5655f
rcen95 <rcen95@gmail.com>
2015-04-19 19:32:46 -0400

    Bayes

---
+++ bayes.py
@@ -0,0 +1,17 @@
+# Bayes classifer 
+# Formula: p(h|D) = arg max [p(D|h) * p(h)]
+
+# Implementation
+class Classifier 
+
+# Read in data and determine count 
+
+# Calculate p(h)
+
+# Calculate conditional probablity p(D|h)
+
+# Returns the class we think the item vector is in
+def classify
+
+	# arg max of [p(D|h) * p(h)]
+	max (results)
