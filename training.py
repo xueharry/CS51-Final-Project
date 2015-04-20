ea7816fb2fd1f48f1e28611c7d270d8bee2ace24
michelleng <michelleng@college.harvard.edu>
2015-04-19 22:39:44 -0400

    ready to zip

--- training.py
+++ training.py
@@ -2,14 +2,34 @@
 import numpy as np
 
 # Training set 1
-movielens_train1 = u1.base
-movielens_test1 = u1.test
-print movielens_train1.shape
-print movielens_test1.shape
 
-# Save to text file (?) - add to each training set
-movielens_train1.to_csv('../data/movielens_train1.csv')
-movielens_test2.to_csv('../data/movielens_test1.csv')
+   # REMEMBER: INCLUDES PREPROCESSING FILE FOR REFERENCE
+   def loadMovieLens1(path=os.getcwd()+'/data/ml-100k/u1.base')
+   def loadMovieLens1(path=os.getcwd()+'/data/ml-100k/u1.test'):
+       
+    # Get titles
+    movies = {}
+    for line in open(path + '/u.item'):
+        (id,title) = line.split('|')[0:2]
+        movies[id] = title
+        
+    # Load data
+    preferences = {}
+    for line in open(path+'/u.data'):
+        (user,movieid,rating,ts) = line.split('\t')
+        preferences.setdefault(user, {})
+        preferences[user][movies[movieid]] = float(rating)
+    return preferences
+   # END OF PREPROCESSING FILE
+   
+  movielens_train1 = u1.base
+  movielens_test1 = u1.test
+  print movielens_train1.shape
+  print movielens_test1.shape
+
+   # Save to text file (?) - add to each training set
+  movielens_train1.to_csv('../data/movielens_train1.csv')
+  movielens_test2.to_csv('../data/movielens_test1.csv')
 
 # Training set 2
 movielens_train2 = u2.base
