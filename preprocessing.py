96325415229f906a0ba426945842abac96ae59d7
rcen95 <rcen95@gmail.com>
2015-04-22 22:28:02 -0400

    Update

--- preprocessing.py
+++ preprocessing.py
@@ -2,13 +2,17 @@ import math
 import numpy as np
 import pandas as pd
 
-# Read in data 
+def load_data(path=os.getcwd()+'/data/ml-100k'):
+
+# Read in file
 rnames = ['user', 'movie', 'rating', 'timestamp']
-df = pd.read_table('u.data', sep='\t', header=None,
+df = pd.read_table(path+'u.data', sep='\t', header=None,
                         names=rnames)
 
 # Matrix of size 1000 x 1700 (rows x cols)
-matrix = [[0 for _ in range(1700)] for _ in range(1000)]
+matrix = [[3 for _ in range(1700)] for _ in range(1000)]
+
+### Update the average rating for user, normalize to remove bias 
 
 # Fill in matrix from data
 for index in df.index: 
@@ -17,6 +21,5 @@ for index in df.index:
     rating = df['rating'][index] 
     matrix[user][movie] = rating
 
-
-
+return matrix
 
