cdf55be8e28838d0ae097a82c5d44321aa4d7731
Harry Xue <wxue@college.harvard.edu>
2015-04-24 21:38:04 -0400

    changed kmeans, not done yet

--- kmeans.py
+++ kmeans.py
@@ -7,40 +7,65 @@ class kmeans:
 
 	def __init__(self, matrix, k):
 		"Initializer takes in a user-movie matrix and the number of clusters"
-
+        self.data = matrix
 		self.k = k
-		#self.counter = 0
 		self.iteration = 0
 		self.num_points_changed = 0 
-		self.sse = 0 
+		self.sse = 0
+		self.num_users = len(self.data)
+		self.num_movies = len(self.data[0]) 
 
-		# List of cluster numbers 
+		# List of cluster numbers, all initialized as -1
 		# TODO: TEST WHETHER THERE ARE ANY -1 IN LIST
-		self.cluster_num = [-1 for i in range(len(matrix[0]))]
+		self.cluster_num = [-1 for i in range(self.num_users)]
 
-		# Select random centroids from remaining points
+		# Initialize with random centroids
 		random.seed()
-		self.centroids = TODO 
-		
+		self.centroids = [[self.data[i][rand] for i in range(1, num_users)]
+							for rand in random.sample(range(num_movies, self.k)]
 		self.assignPointsToCluster()
 
 	def updateCentroids(self):
 		"Determine the centroid of each cluster"
+		num_members = [self.cluster_num.count(i) for i in range(len(self.centroids))]
+		self.centroids = [[sum([self.data[k][i]
+								for i in range(num_movies)
+								if self.cluster_num[i] == centroid])/members[centroid]
+							for k in range(1, len(self.data))]
+						for centroid in range(len(self.centroids))]
 
 	def assignPointToCluster(self,i):
 		"Assign point to cluster"
-		# Keep track of changing points
-		# Add square of distance to sum of squared error
+		minRange = sys.maxsize 
+		clusterNum = -1
 
-		return ClusterNum
+		# Find closest cluster
+		if centroid in range(self.k):
+			d = self.distance(i, centroid)
+			if d < minRange:
+				min = clusterNum = centroid
+		# Keep track of number of points changed
+		if clusterNum != self.cluster_num(i)
+			self.num_points_changed += 1
+
+		# Update sum of square error
+		self.sse += min * min
+
+		return clusterNum
 
 	def assignPointsToCluster(self):
+	    "Assign each of the points to cluster"
+	    self.pointsChanged = 0
+	    self.sse = 0
+	    self.cluster_num = [self.assignPointToCluster(i)
+	    					for i in range(num_users)]
 
-	def distance(self, i, j):
-		"Distance from point i to centroid j"
+
+	def distance(self, i, c):
+		"Distance from point i to centroid c"
 		for n in range (1,len(matrix[0])):
-			sum_squares += [pow(matrix[n][i]-self.centroid[j][n-1]),2]
-		return math.sqrt(sum_squares)
+			sum_squares += [pow(matrix[n][i]-self.centroid[c][n-1]),2]
+    	return math.sqrt(sum_squares)
 
 	def kCluster(self):
 		"Performs clustering"
@@ -51,11 +76,13 @@ class kmeans:
 		while not done:
 			self.updateCentroids()
 			self.assignPointsToCluster()
-			self.iterationNumber += 1
+			self.iteration += 1
 
 			# Check whether clustering is done
-			if float(num_points_changed) / self.dats(#total number of points) < 0.01
+			if float(self.num_points_changed) / self.num_users < 0.01:
 				done = True
+			
 			# Debugging purposes, check SSE
+			print("SSE: %f" % self.sse)
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -4,18 +4,17 @@ import pandas as pd
 
 def load_data(path=os.getcwd()+'/data/ml-100k'):
 
-# Read in file
-rnames = ['user', 'movie', 'rating', 'timestamp']
-df = pd.read_table(path+'u.data', sep='\t', header=None,
-                        names=rnames)
+    # Read in file
+    rnames = ['user', 'movie', 'rating', 'timestamp']
+    df = pd.read_table(path+'u.data', sep='\t', header=None, names=rnames)
 
-# Matrix of size 1000 x 1700 (rows x cols)
-matrix = [[3 for _ in range(1700)] for _ in range(1000)]
+    # Matrix of size 1000 x 1700 (rows x cols)
+    matrix = [[3 for _ in range(1700)] for _ in range(1000)]
 
-### Update the average rating for user, normalize to remove bias 
+    ### Update the average rating for user, normalize to remove bias 
 
-# Fill in matrix from data
-for index in df.index: 
+    # Fill in matrix from data
+    for index in df.index: 
     user = df['user'][index] 
     movie = df['movie'][index] 
     rating = df['rating'][index] 
