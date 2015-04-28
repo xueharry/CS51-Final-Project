1d7a23f436ff3819b68f57fc028b1f2a77c2c8a1
michelleng <michelleng@college.harvard.edu>
2015-04-27 23:14:20 -0400

    harry to the rescue

---
+++ nptesting.py
@@ -0,0 +1,23 @@
+import math
+import random
+'''import NPKMEANS SEPARATE FILE'''
+'''import NPTEST_PROCESSING SEPARATE FILE'''
+import sys
+import numpy as np
+
+# Call matrix of users' ratings from u.data
+matrix = ''' from the thing '''
+
+# Call matrix of test ratings from u1.test
+test = ''' from the other thing '''
+
+# Index out test_user's list of ratings from matrix
+new_
+
+# Test results using root-mean-square deviation
+x = np.array([1.2,5.0,2.1])
+y = np.array([3.4,4.2,1.0])
+
+rmsd = np.sqrt((((x.astype(float))-(y.astype(float)))**2)/2)
+print rmsd
+print rmsd.mean()
