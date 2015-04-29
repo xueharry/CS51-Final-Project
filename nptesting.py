2f0d165dff1c95700d12e04784fb9d04b2f67c2b
michelleng <michelleng@college.harvard.edu>
2015-04-29 14:49:21 -0400

    working on testing

--- nptesting.py
+++ nptesting.py
@@ -6,39 +6,48 @@ from npkmeans import *
 import sys
 import numpy as np
 
-# Find predictions from test_user's cluster
-def find_centroid (user_id):
-    "Gets the user's kmeans cluster by user id and accesses the centroid (with mean ratings for every movie) for predictions"
-    km = kmeans(preprocessing.load_data("u.data", 1), 100)
-    kmeans_return = km.kCluster()
-    user_cluster = kmeans_return[0][user_id-1]
-    predict = kmeans_return[1][user_id-1]
-    return predict
-
 # Evaluate accuracy of predictions
 def rmsd_element (num1, num2):
     "Finds the rmsd between two movie ratings"
-    addition = 0.0
-    return ((num1.astype(float))-(num2.astype(float)))**2
+    return ((float(num1))-(float(num2)))**2
       
 def one_user_rmsd (m1, m2):
     "Finds the average rmsd for all of one user's movie ratings"
+    addition = 0.0
+    i = 0
     for index, (a, b) in enumerate(zip(m1, m2)):
-        if b != 0:
+        if b.any != 0:
             addition = addition+rmsd_element(a, b)
             ++i
-        rmsd_inside = addition/i
-        rmsd = np.sqrt(rmsd_inside)
-        return rmsd
+    rmsd_inside = addition/float(i)
+    rmsd = np.sqrt(rmsd_inside)
+    return rmsd
 
 def calculate ():
     "Finds the average rmsd for all users"
-    # kmeans_matrix, test_matrix
+    km = kmeans(preprocessing.load_data("u.data", 1), 100)
+    kmeans_return = km.kCluster()
+    user_cluster = kmeans_return[0]
+    centroids = kmeans_return[1]
+
     average_rmsd = 0.0
     test_matrix = testing_preprocessing.load_data("u1.test", 1)
+    number_test_users = 0
+    print test_matrix
     for test_user in test_matrix:
-        actual = test_matrix[test_user]
-        predictions = find_centroid(test_user)
+        actual = test_matrix[test_user-1]
+        predictions = centroids[user_cluster[test_user-1]]
         average_rmsd = average_rmsd+(one_user_rmsd (predictions, actual))
-    final_evaluation = average_rmsd/(len(test_matrix))
+        number_test_users = 1+number_test_users
+        "fix this- gives total number of users, not number of users who rated in test file"
+    final_evaluation = average_rmsd/float(number_test_users)
+    print "actual"
+    print actual
+    print "predictions"
+    print predictions
+    print "average_rmsd"
+    print average_rmsd
+    print "number test users"
+    print number_test_users
+    print "final_evaluation"
     print final_evaluation
