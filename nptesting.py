e1cb5e732d0c17d093e9764cec08c1810436d8e2
michelleng <michelleng@college.harvard.edu>
2015-04-30 18:51:50 -0400

    TESTING WORKS BITCHES

--- nptesting.py
+++ nptesting.py
@@ -15,17 +15,24 @@ def one_user_rmsd (m1, m2):
     addition = 0.0
     i = 0
     for index, (a, b) in enumerate(zip(m1, m2)):
-        zipped = (a, b)
         if b <> 0:
         # if zipped[1] != 0:
         # if b != 0:
         # if b.any != 0:
         # if np.nonzero(b):
         #if 0 in (a,b):
-            addition = addition+rmsd_element(zipped[0], zipped[1])
-            # addition = addition+rmsd_element(a, b)
+            # addition = addition+rmsd_element(zipped[0], zipped[1])
+            addition = addition+rmsd_element(a, b)
             i = i+1
+            print "index"
+            print index
+            print "b is not 0"
+            print "addition"
+            print addition
         else:
+            print "b is 0"
+            print "addition"
+            print addition
             continue
     if i==0:
         rmsd_inside = 0
@@ -40,20 +47,24 @@ def calculate ():
     kmeans_return = km.kCluster()
     user_cluster = kmeans_return[0]
     centroids = kmeans_return[1]
-
-    total_rmsd = 0.0
     test_matrix = preprocessing.testing_load_data("u1.test", 1)
+    print "test_matrix"
+    print test_matrix
+    
+    total_rmsd = 0.0
     number_test_users = 0
+    user_id = 0
     for test_user in test_matrix:
-        actual = test_matrix[0][test_user-1]
-        print "actual"
-        print actual
-        predictions = centroids[user_cluster[test_user-1]]
+        actual = test_user
+        predictions = centroids[user_cluster[user_id]]
         one_rmsd = one_user_rmsd (predictions, actual)
         total_rmsd = total_rmsd+one_rmsd
+        user_id = user_id + 1
         if one_rmsd==0:
+            print "one_rmsd = 0"
             continue
         else:
+            print "one_rmsd is not 0"
             number_test_users = number_test_users+1
             "attempted to fix users to be number of users in test file, not number of users in array (which is all of them)"
     final_evaluation = total_rmsd/float(number_test_users)
@@ -65,5 +76,7 @@ def calculate ():
     print total_rmsd
     print "number test users"
     print number_test_users
+    print "user_id"
+    print user_id
     print "final_evaluation"
     print final_evaluation
