77a937d82bc60b26f79031c08046e50a5d4647bb
Harry Xue <wxue@college.harvard.edu>
2015-04-29 22:16:06 -0400

    merge

--- preprocessing.py
+++ preprocessing.py
@@ -16,24 +16,21 @@ def load_data(file_name, npy, path=os.getcwd()+'/data/ml-100k/'):
     # Matrix of size num_users x num_movies (rows x cols)
     matrix = [[3 for _ in range(num_movies)] for _ in range(num_users)]
 
-    ### Update the average rating for user, normalize to remove bias 
-
     # Fill in matrix from data
     for index in df.index: 
         user = df['user'][index] 
         movie = df['movie'][index] 
         rating = df['rating'][index] 
         matrix[user-1][movie-1] = rating
-        
-    # matrix = [[1,2,3,4],[4,3,3,1],[5,5,5,5],[1,4,3,2]]
 
-    if npy==1:
+    # Check for which version of kmeans to use
+    if npy == 1:
         print('numpy kmeans')
         m = np.array(matrix)
         m.astype(float)
     else:
         print('slow kmeans')
-        m=matrix
+        m = matrix
     
     return m
 
