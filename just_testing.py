f5234173bedcb42c69aaf9e6ecd0813c029a88a2
michelleng <michelleng@college.harvard.edu>
2015-04-30 18:57:44 -0400

    got rid of the prints

--- just_testing.py
+++ just_testing.py
@@ -16,9 +16,23 @@ def one_user_rmsd (m1, m2):
     i = 0
     for index, (a, b) in enumerate(zip(m1, m2)):
         if b <> 0:
+        # if zipped[1] != 0:
+        # if b != 0:
+        # if b.any != 0:
+        # if np.nonzero(b):
+        #if 0 in (a,b):
+            # addition = addition+rmsd_element(zipped[0], zipped[1])
             addition = addition+rmsd_element(a, b)
             i = i+1
+            print "index"
+            print index
+            print "b is not 0"
+            print "addition"
+            print addition
         else:
+            print "b is 0"
+            print "addition"
+            print addition
             continue
     if i==0:
         rmsd_inside = 0
@@ -29,24 +43,35 @@ def one_user_rmsd (m1, m2):
 
 def calculate ():
     "Finds the average rmsd for all users"
-    km = kmeans(preprocessing.load_data("u.data", 1), 100)
-    kmeans_return = km.kCluster()
-    user_cluster = kmeans_return[0]
-    centroids = kmeans_return[1]
-    test_matrix = preprocessing.testing_load_data("u1.test", 1)
-    
+    test_matrix = [[5,0,0,0,4],[0,0,0,0,0], [1,2,3,4,5]]
+    predict_matrix = [[3,4,5,4,3], [3,4,3,2,1], [1,1,1,1,1]]
+
     total_rmsd = 0.0
     number_test_users = 0
     user_id = 0
     for test_user in test_matrix:
         actual = test_user
-        predictions = centroids[user_cluster[user_id]]
+        predictions = predict_matrix[user_id]
         one_rmsd = one_user_rmsd (predictions, actual)
         total_rmsd = total_rmsd+one_rmsd
         user_id = user_id + 1
         if one_rmsd==0:
+            print "one_rmsd = 0"
             continue
         else:
+            print "one_rmsd is not 0"
             number_test_users = number_test_users+1
+            "attempted to fix users to be number of users in test file, not number of users in array (which is all of them)"
     final_evaluation = total_rmsd/float(number_test_users)
-    return final_evaluation
+    print "actual"
+    print actual
+    print "predictions"
+    print predictions
+    print "total_rmsd"
+    print total_rmsd
+    print "number test users"
+    print number_test_users
+    print "user_id"
+    print user_id
+    print "final_evaluation"
+    print final_evaluation
